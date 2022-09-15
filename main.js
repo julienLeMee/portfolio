ba33794bc80ef01929de9396fd4ad550bb5245c1
Julien Le Mee <julemee@gmail.com>
2022-09-15 17:33:16 -0400

    chnage style global

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-import { Controller } from "@hotwired/stimulus"
-import Typed from "typed.js"
+// import { Controller } from "@hotwired/stimulus"
+// import Typed from "typed.js"
 
-// Connects to data-controller="typed-js"
-export default class extends Controller {
-  connect() {
-    new Typed(this.element, {
-      strings: ["Have a break", "Have a KitKat"],
-      typeSpeed: 80,
-      loop: true
-    })
+// // Connects to data-controller="typed-js"
+// export default class extends Controller {
+//   connect() {
+//     new Typed(this.element, {
+//       strings: ["Have a break", "Have a KitKat"],
+//       typeSpeed: 80,
+//       loop: true
+//     })
+//   }
+// }
+
+const navbar = document.getElementById('navbar');
+const navlinks = document.querySelectorAll('.nav-link');
+const barre = document.querySelector('.barre');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add('scrolled');
+    navlinks.forEach((link) => {
+      link.style = 'color: #DFDBD9';
+    });
+  } else {
+    navbar.classList.remove('scrolled');
+    navlinks.forEach((link) => {
+      link.style = 'color: #rgba(21, 21, 21)';
+    });
   }
-}
+});
