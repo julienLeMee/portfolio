0a0440b429d3e64a5a840920504d24a76953b1b2
Julien Le Mee <julemee@gmail.com>
2022-12-05 17:12:09 -0500

    update dependencies

--- main.js
+++ main.js
@@ -170,6 +170,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
